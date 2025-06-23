cf53c5ce5f53dce13d6fc60ebf1102a802a56644
Sehab Ahmed <sehab@MacBook-Pro-Sehab.local>
2025-06-23 11:59:49 +0200

    google and github authentication setup successfully

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,8 +1,32 @@
-const DashboardPage = () => {
+import { authOptions } from "@/app/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
-    <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
-    </div>
+    <>
+      {session?.user && (
+        <div>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          {session.user?.email && (
+            <h1 className="text-4xl text-center mt-10">
+              Email: {session?.user?.email}
+            </h1>
+          )}
+          <Image
+            src={session?.user?.image as string}
+            alt="user image"
+            width={100}
+            height={100}
+            className="rounded-full mx-auto mt-6"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
